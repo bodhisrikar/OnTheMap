d720265a5bd25fa6f16da52c7a9c9de63ea9748d
Srikar Thottempudi <srikarthottempudi@srikars-air.home>
2019-03-25 18:25:06 -0400

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Srikar Thottempudi on 3/25/19.
+//  Copyright © 2019 Srikar Thottempudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
